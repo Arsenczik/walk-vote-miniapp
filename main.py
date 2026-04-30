79e4396a312c9fdb56d71ec52c0da5036040a0ef
Arsenczik <130061020+Arsenczik@users.noreply.github.com>
2026-04-30 16:57:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-from typing import List
+from flask import Flask, send_from_directory
+from api import api
 
-# Модель участника
-class Participant(BaseModel):
-    user_id: int
-    username: str
-    status: str  # "going", "maybe", "declined"
+app = Flask(__name__)
+app.register_blueprint(api, url_prefix="/api")
 
-# Данные ивента (пока в памяти)
-event_data = {
-    "title": "Вечерняя прогулка по центру",
-    "location": "Парк Горького, у главного входа",
-    "time": "19:00, Суббота",
-    "participants": []
-}
+@app.route("/")
+def index():
+    return send_from_directory(".", "index.html")
 
-@app.get("/api/event")
-async def get_event():
-    return event_data
-
-@app.post("/api/event/join")
-async def join_event(person: Participant):
-    # Удаляем старый статус пользователя, если он уже кликал
-    event_data["participants"] = [p for p in event_data["participants"] if p['user_id'] != person.user_id]
-    
-    # Добавляем новый статус
-    event_data["participants"].append(person.dict())
-    return {"status": "updated", "count": len(event_data["participants"])}
+if __name__ == "__main__":
+    app.run()
