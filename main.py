8628edc3796e35c9f8687fc4fa838de7c414cc41
Arsenczik <130061020+Arsenczik@users.noreply.github.com>
2026-04-30 17:08:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from flask import Flask, send_from_directory
+from flask import Flask
 from api import api
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 app.register_blueprint(api, url_prefix="/api")
 
 @app.route("/")
 def index():
-    return send_from_directory(".", "index.html")
+    return app.send_static_file("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    
