f4a46dbf566a6f8f07f2c58e61aee1cf25ac6f88
Arsenczik <130061020+Arsenczik@users.noreply.github.com>
2026-04-30 18:58:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_file
 from api import api_bp
 import os
 
@@ -7,14 +7,8 @@ app.register_blueprint(api_bp)
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
-
-@app.route('/<path:path>')
-def static_files(path):
-    if os.path.exists(path):
-        return send_from_directory('.', path)
-    return send_from_directory('.', 'index.html')
+    return send_file('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
